a0586e48438e178b05d1a83681ba6c1bf6c4498a
kkings <kyle_kingsbury@epam.com>
2017-02-01 11:24:01 -0500

    Adds nuget reference to core accelerator

--- src/Feature/CDN/code/Properties/AssemblyInfo.cs
+++ src/Feature/CDN/code/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Sitecore.Feature.Azure")]
+[assembly: AssemblyTitle("Azure Provider for Sitecore.Foundation.CDN")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("EPAM Systems")]
-[assembly: AssemblyProduct("Sitecore.Feature.Azure")]
-[assembly: AssemblyCopyright("Copyright © EPAM Systems 2017")]
+[assembly: AssemblyCompany("Kyle Kingsbury")]
+[assembly: AssemblyProduct("Sitecore.Feature.CDN.Azure")]
+[assembly: AssemblyCopyright("Copyright © Kyle Kingsbury 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.2")]
+[assembly: AssemblyFileVersion("0.1.2")]
